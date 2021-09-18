f5380d4f1700ea90f7664df5f03d62f6536b349e
OrdoYe <90972671+OrdoYe@users.noreply.github.com>
2021-09-18 23:08:26 +0800

    Update Prework 1 Regression.py

--- Prework 1 Regression.py
+++ Prework 1 Regression.py
@@ -1,158 +1,44 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 pip install pandas_datareader
-
-
-# In[2]:
-
-
 pip install --upgrade --no-deps statsmodels
-
-
-# In[3]:
-
-
 pip install patsy
 
-
-# In[4]:
-
-
 import pandas_datareader.data as dtr
-
-
-# In[5]:
-
-
 import numpy as np
-
-
-# In[6]:
-
-
 import statsmodels.formula.api as sm
 
-
-# In[7]:
-
-
 tickers = ['msft','tsla','nvda','pypl','pep','^gspc']
-
-
-# In[8]:
-
-
 D = dtr.DataReader(tickers,'yahoo')
-
-
-# In[9]:
-
-
 type(D)
-
-
-# In[10]:
-
-
 D.tail()
 
-
-# In[11]:
-
-
 P = D['Adj Close']
-
-
-# In[12]:
-
-
 P.tail()
 
-
-# In[13]:
-
-
 Returns = P / P.shift(1) - 1
-
-
-# In[14]:
-
-
 Returns.tail()
 
-
-# In[15]:
-
-
 R = P.pct_change()
-
-
-# In[16]:
-
-
 R.tail()
 
-
-# In[17]:
-
-
 R2 = R.tail(100)
-
-
-# In[18]:
-
-
 R2.columns
-
-
-# In[19]:
-
-
 R2.rename(columns = {'^gspc':'SnP'},inplace=True)
-
-
-# In[20]:
-
-
 R2.tail()
 
-
-# In[22]:
-
-
-results = sm.ols(formula = 'msft ~ SnP', data=R2).fit()
-
-
-# In[23]:
-
-
-results.params
-
-
-# In[24]:
-
+results1 = sm.ols(formula = 'msft ~ SnP', data=R2).fit()
+results1.params
+results2 = sm.ols(formula = 'tsla ~ SnP', data=R2).fit()
+results2.params
+results3 = sm.ols(formula = 'nvda ~ SnP', data=R2).fit()
+results3.params
+results4 = sm.ols(formula = 'pvpl ~ SnP', data=R2).fit()
+results4.params
+results5 = sm.ols(formula = 'pep ~ SnP', data=R2).fit()
+results5.params
 
 mystocks = R.columns[:5]
-
-
-# In[25]:
-
-
 mystocks
-
-
-# In[26]:
-
-
 type(mystocks)
-
-
-# In[27]:
-
 
 betalist = []
 for mystock in mystocks:
@@ -161,30 +47,9 @@ for mystock in mystocks:
     results = sm.ols(formula = myformula, data=R2).fit()
     print(results.params['SnP'])
     betalist.append(results.params['SnP'])
-
-
-# In[28]:
-
-
 betalist
 
-
-# In[29]:
-
-
 betavec = np.array(betalist)
-
-
-# In[30]:
-
-
 notional = 100
 notional * betavec
 sum(notional + betavec)
-
-
-# In[ ]:
-
-
-
-
